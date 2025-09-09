2927970f176992ed2dae41a6b743f20454a217a8
David A. Symons <david@git.id.au>
2025-09-09 19:06:42 +0100

    feat: add caveats method to Homebrew formula

--- Formula/try.rb
+++ Formula/try.rb
@@ -11,6 +11,20 @@ class Try < Formula
     bin.install "try.rb" => "try"
   end
 
+  def caveats
+    <<~EOS
+      To set up try with your shell, add one of the following to your shell configuration:
+
+      For bash/zsh:
+        eval "$(try init ~/src/tries)"
+
+      For fish:
+        eval "(try init ~/src/tries | string collect)"
+
+      You can change ~/src/tries to any directory where you want your experiments stored.
+    EOS
+  end
+
   test do
     system "#{bin}/try", "--help"
   end
