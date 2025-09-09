603f4915413fd68d26a3649bcad86bd27e4d6398
David A. Symons <david@git.id.au>
2025-09-09 12:35:46 +0100

    Add Homebrew tap support

---
+++ Formula/try.rb
@@ -0,0 +1,17 @@
+class Try < Formula
+  desc "Fresh directories for every vibe - lightweight experiments for people with ADHD"
+  homepage "https://github.com/o6uoq/try"
+  url "https://github.com/o6uoq/try/archive/refs/heads/main.tar.gz"
+  sha256 "dd3753f38b5c35597c8c2528a19efd7e4289bbbe977d18e2e299a2a57b393a8e"
+  version "main"
+
+  depends_on "ruby"
+
+  def install
+    bin.install "try.rb" => "try"
+  end
+
+  test do
+    system "#{bin}/try", "--help"
+  end
+end
